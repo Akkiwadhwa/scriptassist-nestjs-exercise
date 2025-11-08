ad3c7df89862ee277fa13a63d138fbfa5eaeb5d6
Akkiwadhwa <akkiwadhwa1995@gmail.com>
2025-11-09 00:27:32 +0530

    Add type definition for refresh_token_hash column in User entity

--- src/modules/users/entities/user.entity.ts
+++ src/modules/users/entities/user.entity.ts
@@ -29,7 +29,7 @@ export class User {
   @UpdateDateColumn({ name: 'updated_at' })
   updatedAt: Date;
 
-  @Column({ name: 'refresh_token_hash', nullable: true })
+  @Column({ name: 'refresh_token_hash', type: 'varchar', nullable: true })
   @Exclude({ toPlainOnly: true })
   refreshTokenHash?: string | null;
 }
